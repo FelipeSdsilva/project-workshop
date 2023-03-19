652f92e5e98c643a1409b96ae12b2516670d0522
Felipe Sousa <felipe.fps09@hotmail.com>
2023-03-19 17:20:24 -0300

    Associate products with order, include method getOrders

--- backend/src/main/java/com/project/workshop/entities/Product.java
+++ backend/src/main/java/com/project/workshop/entities/Product.java
@@ -29,4 +29,15 @@ public class Product implements Serializable {
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
+    public Set<Order> getOrders() {
+        Set<Order> orders = new HashSet<>();
+        for (OrderItem orderItem : items) {
+            orders.add(orderItem.getOrder());
+        }
+        return orders;
+    }
 }
