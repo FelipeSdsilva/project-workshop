bdf94fb32b8a6d3b08dcc077d111db71a071b4e3
Felipe Sousa <felipe.fps09@hotmail.com>
2023-03-25 20:09:03 -0300

    Implements class PaymentDTO

---
+++ backend/src/main/java/com/project/workshop/dto/PaymentDTO.java
@@ -0,0 +1,19 @@
+package com.project.workshop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentDTO implements Serializable {
+
+    private Long id;
+    private Instant moment;
+}
