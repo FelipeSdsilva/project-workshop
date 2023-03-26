e612a0676e5e7128fa9895af2199ef8fb4b2a988
Felipe Sousa <felipe.fps09@hotmail.com>
2023-03-26 09:08:28 -0300

    Initialize exceptions in package Resource, add class FieldMessage

---
+++ backend/src/main/java/com/project/workshop/resources/exceptions/FieldMessage.java
@@ -0,0 +1,19 @@
+package com.project.workshop.resources.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FieldMessage implements Serializable {
+
+    private String fieldName;
+    private String message;
+
+}
