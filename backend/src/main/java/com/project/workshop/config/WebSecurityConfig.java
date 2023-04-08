a7900c21c640c25aa391a93dd0833ac3c3513f52
Felipe Sousa <felipe.fps09@hotmail.com>
2023-04-08 12:09:27 -0300

    Initialize class WebSecurityConfig

---
+++ backend/src/main/java/com/project/workshop/config/WebSecurityConfig.java
@@ -0,0 +1,11 @@
+package com.project.workshop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebSecurityConfig {
+
+
+}
